c078aea5edf1bcb18a66f6e1f316d2dd598211e6
VaitoSoi <duyduy15456@gmail.com>
2025-12-26 00:08:09 +0700

    feat(be): add deps to check perm

--- backend/lib/dependency.py
+++ backend/lib/dependency.py
@@ -3,11 +3,11 @@ from typing import Annotated
 from fastapi import Cookie, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .db import SlicedUser, get_session, get_user_session
+from .db import SlicedUser, can_see_entry, get_session, get_user_session
 from .execption import SessionNotFound, UserNotFound
 
 
-async def get_user(session_id: Annotated[str, Cookie()], session: Annotated[AsyncSession, get_session]):
+async def get_user(session_id: Annotated[str, Cookie()], session: Annotated[AsyncSession, Depends(get_session)]):
     try:
         user_session = await get_user_session(session_id, session)
         return user_session.user
@@ -21,3 +21,11 @@ async def require_user(user: Annotated[SlicedUser, Depends(get_user)]):
             detail={"message": "no auth cookie"}
         )
     return user
+
+# seeable = can see
+async def check_can_see_id(user: Annotated[SlicedUser, Depends(get_user)], id: str):
+    if not await can_see_entry(user.id, id):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={"message": "you are not allowed to view this entry"}
+        )
