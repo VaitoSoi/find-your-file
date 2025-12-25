7fedd9db91e91800827718b8cac19256a3f6b607
VaitoSoi <duyduy15456@gmail.com>
2025-12-26 00:07:55 +0700

    feat(be): add dotenv lib

--- backend/lib/env.py
+++ backend/lib/env.py
@@ -1,6 +1,13 @@
 from os import getenv
 from typing import cast
 
+try:
+    import dotenv
+
+    dotenv.load_dotenv()
+except ImportError:
+    ...
+
 DB_URL = getenv("DB_URL", "sqlite+aiosqlie:///data/database.db")
 REDIS_URL = getenv("REDIS_URL")
 
@@ -10,9 +17,9 @@ BUCKET_ACCESS_KEY = getenv("BUCKET_ACCESS_KEY")
 BUCKET_SECRET_KEY = getenv("BUCKET_SECRET_KEY")
 BUCKET_REGION = getenv("BUCKET_REGION")
 
-USE_HASH = getenv("USE_HASH", "false").lower() in ["false", "0", "no"]
+USE_HASH = getenv("USE_HASH", "true").lower() in ["true", "1", "yes"]
 
-DEFAULT_MAX_SESSION_TIME = 30 * 24 * 60 * 60 # 30 days
+DEFAULT_MAX_SESSION_TIME = 30 * 24 * 60 * 60  # 30 days
 try:
     MAX_SESSION_TIME = int(getenv("MAX_SESSION_TIME", DEFAULT_MAX_SESSION_TIME))
 except TypeError:
