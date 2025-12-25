84695038073172875169b5a4a726568e742bcf19
VaitoSoi <duyduy15456@gmail.com>
2025-12-26 01:42:48 +0700

    feat(fe): add main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import { ThemeProvider } from "./components/Theme";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return <ThemeProvider>
+        <div className="w-screen h-screen flex flex-col dark:bg-zinc-900">
+            <Header />
+            <div className="flex-1 flex flex-row">
+                <Sidebar />
+                <div className="flex-1 rounded-tl-4xl dark:bg-black/30"></div>
+            </div>
+        </div>
+    </ThemeProvider>;
 }
-
-export default App
